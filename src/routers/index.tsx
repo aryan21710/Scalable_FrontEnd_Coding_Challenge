7ed27f463e741c57f128c703bf13eb746e525931
aryan.singh <aryan.singh@thoughtworks.com>
2021-03-04 19:06:30 +0530

    CHANGED THE DEFAULT RISK LEVEL TO 3 FROM 10

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -18,7 +18,7 @@ import { useFetchApiData } from '../customHooks/useFetchApiData';
 
 
 const AppRoutes:()=>JSX.Element = () => {
-    const [cone, setCone] = useState({});
+    const [cone, setCone] = useState<ICone>({ mu: 0, riskLevel: 0, sigma: 0 });
     const [cones, setCones] = useState<ICone[]>([]);
 
     const [riskLevel, setRiskLevel] = useState<number>(defaultRiskLevel);
